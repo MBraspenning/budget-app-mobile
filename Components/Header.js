f30234b55aa9a8a3a61719ad4598800fa39c1997
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-05-25 21:50:41 +0200

    initial commit

---
+++ Components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Header extends React.Component
+{
+    render()
+    {
+        return (
+            <Text>Budget App</Text>
+        );
+    }
+}
