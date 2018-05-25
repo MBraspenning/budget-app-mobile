fd27e18fd8f2c6b74cb7b1502bc48ef98bd39a9c
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-05-25 22:20:21 +0200

    adds Header component

--- Components/Header.js
+++ Components/Header.js
@@ -3,10 +3,44 @@ import { Text } from 'react-native';
 
 export default class Header extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = { month: '' }
+    }
+    
+    componentDidMount() 
+    {
+        this.getCurrentMonth();
+    }
+    
+    getCurrentMonth() 
+    {        
+        const months = [
+            'January', 
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ];
+        
+        let monthIndex = new Date().getMonth();
+        let currentMonth = months[monthIndex];
+        
+        this.setState({ month: currentMonth });        
+    }
+    
     render()
     {
         return (
-            <Text>Budget App</Text>
+            <Text>Budget For {this.state.month} :</Text>
         );
     }
 }
