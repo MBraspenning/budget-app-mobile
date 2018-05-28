db76643e1fa51493521ed0ecb527e71559e33ae9
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-05-28 20:31:03 +0200

    Initiates Input Component

---
+++ Components/Input.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+export default class Input extends React.Component
+{
+    render()
+    {
+        return (
+            <View>
+                <Text>
+                    Hello world!
+                </Text>
+            </View>
+        )
+    }
+}
